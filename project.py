57e7004001b024a3828e1bb337cac95b791954fa
Tymofii Tarasenko <116552404+tymmko@users.noreply.github.com>
2023-02-08 16:46:09 +0200

    second_rule upd

--- project.py
+++ project.py
@@ -52,16 +52,32 @@ def second_rule(board):
 "*76**91**","**83**5**", "*4**8**3*", "***7*1**8",\
 "2**5***4*", "*6***2**9"])
     True
-    >>> validate_board(["**** ****", "***1 ****", "**  3****",\
-"* 4 1****", "     9 5 ", " 6  83  *", "3   4  **", "  8  2***", "  2  ****"]
+    >>> second_rule(["*1**4***5","**3***27*","4***2**6*","*76**91**",\
+"**83**5**","*4**8**3*","***7*1**8",\
+"2**5***4*","*6**2***9"])
+    False
+    >>> second_rule(["**** ****", "***1 ****", "  3****",\
+"* 4 1****", "     9 5 ", " 6  89  *", "3   1  ","  8  2***", "  2  ****"])
+    False
+    >>> second_rule(["**** ****",\
+                    "***1 ****",\
+                    "  3****",\
+                    "* 4 1****",\
+                    "     9 5 ",\
+                    " 6  89  *",\
+                    "3   1  ",\
+                    "  8  2***",\
+                    "  2  ****"])
     False
     """
-    if not first_rule(board):
-        return False
-    if not second_rule(board):
-        return False
-    if not third_rule(board):
-        return False
+    res = [[] for x in range(1,10)]
+    for line in board:
+        for num, element in enumerate(line):
+            res[num].append(element)
+    for boardy in res:
+        for k in range(1,10):
+            if boardy.count(str(k))>1:
+                return False
     return True
 
 def third_rule(board):
