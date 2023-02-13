59f0bc0564d4932a2a1804b8f9a5bc6de26c787c
niuta24 <syvkova.pn@ucu.edu.ua>
2023-02-13 10:51:15 +0300

    Done

--- project.py
+++ project.py
@@ -1,11 +1,10 @@
-def third_rule(board):
-    """Checking if there is no repeating of numbers in the colours of the field 
-
+def third_rule(board:list) -> bool:
+    """
+    Checking if there is no repeating of numbers in the colours of the field
     Args:
         board (list): field of the game
-
     Returns:
-        bool: True if there is no repeating of numbers in the colours 
+        bool: True if there is no repeating of numbers in the colours
     >>> third_rule(["**** ****", "***1 ****", "** 13****", "* 4  ****",\
 "     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
     False
@@ -16,15 +15,16 @@ def third_rule(board):
     row = 0
     col = 4
     check_lst = []
-    for ind in range(5):
+    for _ in range(5):
         for jnd in range(5):
             if board[row + jnd][col] != " " and board[row + jnd][col] != "*":
                 check_lst.append(int(board[row + jnd][col]))
-        for jnd in range(5):
-            if board[row + 4][col + jnd] != " " and board[row + 4][col + jnd] != "*":
-                check_lst.append(int(board[row + 4][col + jnd]))
+        for jnd in range(4):
+            if board[row + 4][col + jnd + 1] != " " and board[row + 4][col + jnd + 1] != "*":
+                check_lst.append(int(board[row + 4][col + jnd + 1]))
         row += 1
         col -= 1
+        # print(check_lst)
         for elm in check_lst:
             if int(elm) > 9 or int(elm) < 1:
                 return False
